43821c0f1e9e8a10d336dee1da80f7993b554155
Anshudip1995 <dv7.adg@gmail.com>
2025-06-10 18:09:57 +0530

    Add files via upload

---
+++ Football Quiz.py
@@ -0,0 +1,21 @@
+import pandas as pd
+League = (input("Enter the League Name: "))
+df = pd.read_excel(r'C:\Users\anshu\OneDrive\Desktop\Python_projects\Football Quiz.xlsx', League)
+Q = input("The number of Questions is:")
+R = int(Q)
+Row = 0
+Mark = 0
+while Row in range(R):
+    Question = df.iloc[Row,0]
+    Answer = df.iloc[Row,1]
+    print(Question)
+    A = str(input("The answer is:")).title()
+    if A == Answer:
+        Mark = Mark + 1
+    else:
+        print("The answer is incorrect.")
+    Row = Row + 1
+print("The obtained mark is " + str(Mark) + ".")
+
+
+
